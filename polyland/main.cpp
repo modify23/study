8ae3a1c10f19c03bf23a2443af70b23cca21957e
SJ <modify23@naver.com>
2023-07-19 18:12:33 +0900

    class 내용 추가

--- polyland/main.cpp
+++ polyland/main.cpp
@@ -2,54 +2,105 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
+#include <iostream>
+#include <chrono>
 
+using namespace std;
 
 class dayNnight {
-private :
-	int ticket;
+private:
+	int ticket_type;
 
-public : 
+public:
 	int sel_ticket() {
-		printf("권종을 선택하세요\n");
-		printf("1. 주간권\n");
-		printf("2. 야간권\n");
-		scanf_s("%d", &ticket); 
+		cout << "권종을 선택하세요" << endl;
+		cout << "1. 주간권" << endl;
+		cout << "2. 야간권" << endl;
+		cin >> ticket_type;
 
-		while ((ticket != 1) && (ticket != 2)) { 
-			printf("다시 선택해주세요\n"); 
-			scanf_s("%d", &ticket); 
+		while ((ticket_type != 1) && (ticket_type != 2)) {
+			cout << "다시 선택해주세요" << endl;
+			cin >> ticket_type;
 		}
 
-		return ticket;
+		return ticket_type;
 	}
 };
 
 class Birthdate {
-private :
-	int birthdate;
-	int check = 0;
-public : 
+private:
+	int birthdate = 0;
+	int code = 0;
+	int age = 0;
+public:
 	int CustomerBirthdate() {
-		printf("주민번호 7자리를 입력해주세요");
-		scanf_s("%d", &birthdate);
-		check = birthdate % 10;
-		while (birthdate < 1000000 || birthdate > 9999999 || check == 0 || check == 9 ) {
-			printf("잘못된 주민번호입니다. 다시 입력해주세요: ");
-			scanf_s("%d", &birthdate);
-			check = birthdate % 10;
+		cout << "주민번호 앞자리를 입력해주세요 : " << endl;
+		cin >> birthdate;
+		cout << "주민번호 뒷자리 첫번째까지 입력해 주세요 : " << endl;
+		cin >> code;
+		while (birthdate < 100000 || birthdate > 999999 || code < 0 || code > 9) {
+			cout << "잘못된 주민번호입니다. 다시 입력해주세요: " << endl;
+			cin >> birthdate;
+			cin >> code;
 		}
-		printf("check : %d\n", check);
 		return birthdate;
+	}
+
+	int CustomerAge() {
+		auto now = chrono::system_clock::now();
+		time_t end_time = chrono::system_clock::to_time_t(now);
+
+		int birthYear = birthdate / 10000;
+		int birthMonth = (birthdate / 100) % 100;
+		int birthDay = birthdate % 100;
+
+		struct tm birth_tm = { 0 };
+		birth_tm.tm_year = birthYear;
+		birth_tm.tm_mon = birthMonth;
+		birth_tm.tm_mday = birthDay;
+		time_t birth_time = mktime(&birth_tm);
+
+		double seconds = difftime(end_time, birth_time);
+		int years = static_cast<int>(seconds) / (60 * 60 * 24 * 365);
+
+		cout << "Current Time and Date: " << ctime(&end_time);
+		cout << "만나이: " << years << "세" << endl;
+
+		return years;
 	}
 };
 
 class ticket_price {
-	int price;
-	
+private:
+	int price[2][4] = {
+		{56000, 47000, 44000, 44000},
+		{46000, 40000, 37000, 37000}
+	};
+public:
+	void setPrice(int birthdate, int discountType, int ticket_type) {
+
+	}
+
 };
 
 class discount {
-
+private:
+	int discountType = 0;
+public:
+	int set_discountType() {
+		cout << "우대 사항을 선택하세요" << endl;
+		cout << "1. 없음 (나이우대는 자동 처리)" << endl;
+		cout << "2. 장애인" << endl;
+		cout << "3. 국가유공자" << endl;
+		cout << "4. 다자녀" << endl;
+		cout << "5. 임산부" << endl;
+		cin >> discountType;
+		while (discountType < 1 || discountType > 5) {
+			cout << "잘못된 선택입니다. 다시 입력해주세요: ";
+			cin >> discountType;
+		}
+		return discountType;
+	}
 };
 
 class OrderCount {
@@ -59,10 +110,19 @@ class OrderCount {
 int main() {
 	dayNnight d;
 	Birthdate b;
-	int slected_ticket = d.sel_ticket();
+	discount dc;
+	//int selected_ticket = d.sel_ticket();
 	int birth = b.CustomerBirthdate();
+	int time = b.CustomerAge();
+	//int dc_type = dc.set_discountType();
+
+	/*
+	cout << "선택한 티켓: " << selected_ticket << endl;
+	cout << "주민번호: " << birth << endl;
+	cout << "만나이: " << time << "세" << endl;
+	cout << "우대사항: " << dc_type << endl;
+	*/
 	
-	printf("선택한 티켓 : %d\n", slected_ticket);
-	printf("주민번호 : %d\n", birth);
+
 	return 0;
 }
